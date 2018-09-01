3ecb53165a6fb47f8c2e732a8d1791b467b7539b
Chris Fusting <cfusting@gmail.com>
2018-09-01 16:04:19 -0400

    Sanity check

--- examples/polynomial.py
+++ examples/polynomial.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNetCV
+from sklearn.model_selection import KFold
 
 from efs.evolutionary_feature_synthesis import EFSRegressor
+
+seed = 432
+splits = 6
 
 
 def target(x):
@@ -13,7 +18,14 @@ X = np.linspace(-100, 100, 100, endpoint=True)
 y = target(X)
 X = np.expand_dims(X, axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
-efs = EFSRegressor(verbose=3, max_gens=30, normalize=True, num_additions=2, splits=6)
+efs = EFSRegressor(verbose=4, max_gens=1, normalize=True, num_additions=2, splits=splits, seed=seed)
 efs.fit(X_train, y_train)
 score = efs.score(X_test, y_test)
+print('Score: ' + str(score))
+
+
+cv = KFold(n_splits=splits, random_state=seed)
+en = ElasticNetCV(l1_ratio=1, selection='random', cv=cv, random_state=seed, normalize=False)
+en.fit(X_train, y_train)
+score = en.score(X_test, y_test)
 print('Score: ' + str(score))
